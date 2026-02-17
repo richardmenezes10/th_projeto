379b2ef7fcff9047aa15a52a92a0a20775de1703
Richard Menezes <richard_2019sisilva@hotmail.com>
2026-02-17 15:40:45 -0300

    alterações

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 //const express = require('express');
 import prisma from './src/db.js';
+import { disconnect } from 'cluster';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -11,7 +15,116 @@ app.get('/api', async (req, res) => {
     await prisma.$connect();
     const users = await prisma.user.findMany();   
     res.json({ message: 'Hello from the API!', users });
+    await prisma.$disconnect();
 });
+
+app.get('/users', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+
+    const skip = (page - 1) * limit;
+
+    const users = await prisma.user.findMany({
+      skip,
+      take: limit,
+      include: {
+        products: true
+      }
+    });
+
+    const total = await prisma.user.count();
+
+    res.json({
+      page,
+      totalPages: Math.ceil(total / limit),
+      total,
+      data: users
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar usuários' });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password
+      }
+    });
+
+    res.status(201).json(user);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao criar usuário' });
+  }
+});
+
+app.post('/products', async (req, res) => {
+  try {
+    const { name, price, userId } = req.body;
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+        userId
+      }
+    });
+
+    res.status(201).json(product);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao criar produto' });
+  }
+});
+
+app.post('/products', async (req, res) => {
+  try {
+    const { name, price, userId } = req.body;
+
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+        userId
+      }
+    });
+
+    res.status(201).json(product);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao criar produto' });
+  }
+});
+
+app.get('/products', async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        user: true
+      }
+    });
+
+    res.json(products);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao buscar produtos' });
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
---
+++ prisma/migrations/20260215144852_add_user_products_relation/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
